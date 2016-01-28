c3d38953724e76eb54510389b1a8ab38660bca53
Dharwin Perez <dharwin@rocket.la>
2016-01-28 10:47:09 -0600

    Setting to blank when value is utf-8

--- zopy/core.py
+++ zopy/core.py
@@ -151,10 +151,10 @@ class Connection(Properties):
 				for key, value in lead.items():
 					fl = Element("FL", val=key.decode("utf-8"))
 					if type(value) != str:
-						if type(value) in [int,float]:
+						try:
 							fl.text = str(value)
-						else:
-							fl.text = str(value).decode("utf-8")
+						except:
+							fl.text = ""
 					else:
 						fl.text = value.decode("utf-8")
 					row.append(fl)
