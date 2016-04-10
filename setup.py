f99e9cd09eb8cd70dd270a80e86f7e99074579b8
Dharwin Perez <dhararon@hotmail.com>
2016-04-10 15:05:45 -0500

    Fix README.txt error

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ version = '0.2.0'
 setup(name='Zopy',
       version=version,
       description="Zoho API integration for Python",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
